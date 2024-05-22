88da43d3a080283a11cb918bc28a9002fd6efc9c
vascolm <133411688+vascolm@users.noreply.github.com>
2024-05-22 16:41:16 +0100

    Final Dashboard
    Final Analysis
    Fixed Training, Write performances csv

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from gensim.models import Word2Vec
-from gensim.utils import simple_preprocess  # Added import
+from gensim.utils import simple_preprocess
 import pickle
 from projetofinal.preprocessing import compute_avg_embedding, read_pickle
 from projetofinal.train_tools import return_embeedings
@@ -25,13 +25,14 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, class
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
 
 # Apply the theme
 st.set_page_config(
     page_title="Decoding Movements Dashboard",
     page_icon=":chart_with_upwards_trend:",
-    layout="wide",
     menu_items={
         "About": "This app predicts the sector and territory of a financial instrument based on its description."
     },
@@ -39,7 +40,7 @@ st.set_page_config(
 
 # Use markdown for a custom-styled title and subtitle
 st.markdown(
-    "<h1 style='text-align: center; color: white;'>Decoding Movements</h1>",
+    "<h1 style='text-align: center;'>Decoding Movements</h1>",
     unsafe_allow_html=True,
 )
 st.markdown(
@@ -72,7 +73,7 @@ def home():
 
     # Text input for description
     st.subheader("Description")
-    user_input = st.text_area("", value=st.session_state["selected_example"], height=150)
+    user_input = st.text_input("", value=st.session_state["selected_example"], placeholder='Enter a Description')
 
     # Load pre-trained models and other necessary components based on the selected model
     if model_name:
@@ -82,7 +83,7 @@ def home():
     # Predict button
     if st.button("Predict"):
         if user_input and model_name:
-            tokens = user_input.lower().split()  # Simple tokenization, adjust as needed
+            tokens = simple_preprocess(user_input)
             avg_embedding = compute_avg_embedding(tokens, word2vec_model)
             pred_territory, pred_sector, prediction_t_proba, prediction_s_proba = return_embeedings(
                 user_input,
@@ -159,13 +160,20 @@ def home():
             df, encoding_used = try_read_csv(uploaded_file)
         else:
             df = pd.read_excel(uploaded_file, engine="openpyxl")
-        
-        # Store the DataFrame in session state to use later
-        st.session_state['uploaded_df'] = df
-        
-        # Display the first 10 rows of the DataFrame
-        st.write("Preview of uploaded file (first 10 rows):")
-        st.dataframe(df.head(10))
+
+        required_columns = ["DescricaoInstrumento"]
+
+        # Check if the uploaded file contains all required columns
+        if not all(column in df.columns for column in required_columns):
+            st.error(f"The uploaded file does not contain all required columns: {', '.join(required_columns)}")
+        else:
+            # Store the DataFrame in session state to use later
+            st.session_state['uploaded_df'] = df
+            
+            # Display the first 10 rows of the DataFrame
+            st.write("Preview of uploaded file (first 10 rows):")
+            st.dataframe(df.head(10))
+
 
     # Button to trigger prediction
     if 'uploaded_df' in st.session_state and st.button("Predict from File"):
@@ -183,96 +191,141 @@ def home():
             mime="text/csv"
         )
 
-# Função para a página de análises
+
 def analysis():
     st.title("Análises")
     st.write("Nesta página, é possível observar algumas análises sobre o Território e o Setor. No Território, apresentamos os cinco principais registos de cada país, enquanto no Setor disponibilizamos uma tabela com os registos mais frequentes. Tudo isto é realizado através do token.")
 
-    # Carregar dados territoriais
-    territorio_df = pd.read_csv("projetofinal/analysis/territorio_df.csv")
+    st.markdown("## Análise dos Modelos")
 
-    # Carregar um shapefile do mundo
+    # Load performance data
+    performance_df = pd.read_csv('performance.csv')
+
+    # Function to suggest the best models based on the average of selected metrics
+    def suggest_best_models(performance_df, metric_columns):
+        # Calculate the average of selected metrics
+        performance_df['Aggregate Score'] = performance_df[metric_columns].mean(axis=1)
+        # Sort models by average in descending order and get the top 3
+        best_models = performance_df.nlargest(3, 'Aggregate Score')['Model Name'].tolist()
+        return best_models
+
+    # Create a metric selector
+    selected_metric = st.selectbox("Select the metric:", options=['Accuracy', 'Precision', 'Recall'])
+
+    # Define metric columns based on selected metric
+    if selected_metric == 'Accuracy':
+        metric_columns = ['Accuracy Territory', 'Accuracy Sector']
+        metric_name = 'Accuracy'
+    elif selected_metric == 'Precision':
+        metric_columns = ['Precision Territory', 'Precision Sector']
+        metric_name = 'Precision'
+    elif selected_metric == 'Recall':
+        metric_columns = ['Recall Territory', 'Recall Sector']
+        metric_name = 'Recall'
+
+    # Prepare data for the plot
+    data = []
+    for column in metric_columns:
+        data.append(go.Bar(x=performance_df['Model Name'], y=performance_df[column], name=column))
+
+    # Create the figure
+    fig = go.Figure(data)
+
+    # Update layout
+    fig.update_layout(barmode='group',
+                    title=f'{selected_metric} per Model',
+                    xaxis_title='Model',
+                    yaxis_title=f'{selected_metric}')
+
+    # Display the plot
+    st.plotly_chart(fig)
+
+    # Suggest the best models
+    best_models = suggest_best_models(performance_df, metric_columns)
+    st.write(f"Os melhores modelos com base na média entre {metric_name} Territory e {metric_name} Sector são: {', '.join(best_models)}")
+
+
+    # Load territorial data
+    territory_df = pd.read_csv("projetofinal/analysis/territorio_df.csv")
+
+    # Load a world shapefile
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
-    merged = world.merge(territorio_df, how='left', left_on='iso_a3', right_on='Território')
+    merged = world.merge(territory_df, how='left', left_on='iso_a3', right_on='Território')
 
-    # Criar um mapa folium centrado em uma localização inicial
+    # Create a folium map centered at an initial location
     m = folium.Map(location=[0, 0], zoom_start=2)
 
-# Função para lidar com o clique em um país
-    
+    # Function to handle click on a country
+    def click_handler(feature, **kwargs):
+        iso_a3 = feature['iso_a3']
+        top5 = territory_df[territory_df['Território'] == iso_a3].nlargest(5, 'Contagem')
+        popup_text = f"{feature['Descrição do território']} Top 5:\n"
+        if not top5.empty:
+            for _, row in top5.iterrows():
+                popup_text += f"{row['Token']}:{row['Contagem']}\n"
+        return popup_text
 
-    
-    # Adicionar o GeoDataFrame ao mapa folium
+    # Add GeoDataFrame to the folium map
     folium.GeoJson(
         merged,
         name='geojson',
         tooltip=folium.GeoJsonTooltip(fields=['Descrição do território']),
-        style_function=lambda feature: {'fillColor': '#0E7A0D' if feature['properties']['Território'] in territorio_df['Território'].values else '#ffffff', 'color': '#003300'},  # cor de preenchimento para países com tokens no top 5
-        highlight_function=lambda feature: {'weight': 0},  # remove o destaque dos países
-        popup=folium.Popup(lambda feature: click_handler(feature, territorio_df), max_width=400)  # aumentar a largura máxima da caixa de texto do pop-up
+        style_function=lambda feature: {'fillColor': '#0E7A0D' if feature['properties']['Território'] in territory_df['Território'].values else '#ffffff', 'color': '#003300'},  # fill color for countries with tokens in top 5
+        highlight_function=lambda feature: {'weight': 0},  # remove country highlights
+        popup=folium.Popup(lambda feature: click_handler(feature, territory_df), max_width=400)  # increase max width of popup text box
     ).add_to(m)
-    
-    # Função para lidar com o clique em um país
-    def click_handler(feature, **kwargs):
-        iso_a3 = feature['iso_a3']
-        top5 = territorio_df[territorio_df['Território'] == iso_a3].nlargest(5, 'Contagem')
-        popup_text = f"{feature['Descrição do território']} Top 5:\n"
-        if not top5.empty:
-            for _, row in top5.iterrows():
-                popup_text += f"{row['Token']}:{row['Contagem'] }\n"
-        return popup_text
-    
-     # Adicionar manipuladores de eventos de clique para cada país
+
+    # Add click event handlers for each country
     for _, feature in merged.iterrows():
         iso_a3 = feature['iso_a3']
-        top5 = territorio_df[territorio_df['Território'] == iso_a3].nlargest(5, 'Contagem')
+        top5 = territory_df[territory_df['Território'] == iso_a3].nlargest(5, 'Contagem')
         if not top5.empty:
             geojson = folium.GeoJson(
                 feature['geometry'],
-                style_function=lambda feature: {'fillColor': '#0E7A0D', 'color': '#003300'},  # cor de preenchimento para países com tokens no top 5
-                highlight_function=lambda feature: {'weight': 0},  # remove o destaque dos países
+                style_function=lambda feature: {'fillColor': '#0E7A0D', 'color': '#003300'},  # fill color for countries with tokens in top 5
+                highlight_function=lambda feature: {'weight': 0},  # remove country highlights
                 name='geojson',
-                popup=folium.Popup(click_handler(feature), max_width=400)  # aumentar a largura máxima da caixa de texto do pop-up
+                popup=folium.Popup(click_handler(feature), max_width=400)  # increase max width of popup text box
             )
         else:
             geojson = folium.GeoJson(
                 feature['geometry'],
-                style_function=lambda feature: {'fillColor': '#ffffff', 'color': '#000000'},  # remova a cor de preenchimento para países sem lista
-                highlight_function=lambda feature: {'weight': 0},  # remove o destaque dos países
+                style_function=lambda feature: {'fillColor': '#ffffff', 'color': '#000000'},  # remove fill color for countries without list
+                highlight_function=lambda feature: {'weight': 0},  # remove country highlights
                 name='geojson',
-                popup=None  # sem caixa de texto para países sem lista
+                popup=None  # no text box for countries without list
             )
         geojson.add_to(m)
 
-    # Exibir o mapa interativo
-    st.markdown("### Mapa - Análise por Território")
+    # Display the interactive map
+    st.markdown("### Mapa - Análise de tokens pelo Território")
     folium_static(m)
 
+    # Load sector data
+    sector_df = pd.read_csv('projetofinal/analysis/setor_df.csv')
+    sector_df1 = sector_df.drop(['Unnamed: 0','Descrição do Setor Institucional'], axis = 1)
 
-    setor_df = pd.read_csv('projetofinal/analysis/setor_df.csv')
-    setor_df1 = setor_df.drop(['Unnamed: 0','Descrição do Setor Institucional'], axis = 1)
-    # Função para exibir todos os registros para um determinado setor
-    def registros_por_setor(setor):
-        return setor_df1[setor_df['Setor Institucional'] == setor]
+    # Function to display all records for a given sector
+    def records_per_sector(sector):
+        return sector_df1[sector_df['Setor Institucional'] == sector]
 
-    # Função para atualizar a lista de registros quando um novo setor é selecionado
-    def atualizar_registros(setor_selecionado):
-        st.write(f"Registros para {setor_selecionado} ({setor_df.loc[setor_df['Setor Institucional'] == setor_selecionado, 'Descrição do Setor Institucional'].iloc[0]}):")
-        registros = registros_por_setor(setor_selecionado)
-        st.write(registros)
+    # Function to update the list of records when a new sector is selected
+    def update_records(selected_sector):
+        st.write(f"Resgitos do {selected_sector} ({sector_df.loc[sector_df['Setor Institucional'] == selected_sector, 'Descrição do Setor Institucional'].iloc[0]}):")
+        records = records_per_sector(selected_sector)
+        st.write(records)
 
+    # Get the unique list of institutional sectors
+    st.markdown("### Análise de Tokens por Sector")
+    sectors = sector_df1['Setor Institucional'].unique()
 
-    # Obter a lista única de setores institucionaiss
-    st.markdown("### Análise por Setor")
+    # Create the dropdown menu to select the sector
+    selected_sector = st.selectbox("Selecione o sector:", sectors, index=0)
 
-    setores = setor_df1['Setor Institucional'].unique()
+    # Define the callback function to update records when a new sector is selected
+    update_records(selected_sector)
 
-    # Criar o menu suspenso para selecionar o setor
-    setor_selecionado = st.selectbox("Selecione o setor:", setores, index=0)
-
-    # Definir a função de retorno de chamada para atualizar os registros quando um novo setor é selecionado
-    atualizar_registros(setor_selecionado)
 
 
 def encode_target(label, category_mapping):
@@ -308,6 +361,10 @@ def training():
         "Upload your file (CSV or Excel) containing multiple instances", type=["csv", "xlsx"], key="bulk-file-uploader"
     )
 
+    required_columns = [
+        "DescricaoInstrumento", "SetorInstitucionalCon", "TerritorioCon"
+    ]
+
     # Process bulk upload
     if bulk_uploaded_file is not None:
         if bulk_uploaded_file.type == "text/csv":
@@ -315,12 +372,16 @@ def training():
         else:
             bulk_df = pd.read_excel(bulk_uploaded_file, engine="openpyxl")
 
-        # Display the first 10 rows of the DataFrame
-        st.write("Preview of uploaded file (first 10 rows):")
-        st.dataframe(bulk_df.head(10))
+        # Check if the uploaded file contains all required columns
+        if not all(column in bulk_df.columns for column in required_columns):
+            st.error(f"The uploaded file does not contain all required columns: {', '.join(required_columns)}")
+        else:
+            # Display the first 10 rows of the DataFrame
+            st.write("Preview of uploaded file (first 10 rows):")
+            st.dataframe(bulk_df.head(10))
 
-        # Append bulk data to existing data and proceed with retraining
-        st.session_state['bulk_uploaded_df'] = bulk_df
+            # Append bulk data to existing data and proceed with retraining
+            st.session_state['bulk_uploaded_df'] = bulk_df
 
 
     
@@ -505,6 +566,29 @@ def training():
         axes[1].set_ylabel('Actual')
 
         st.pyplot(fig)
+
+        st.write("Retraining and evaluation completed.")
+
+        # Update performance.csv
+        performance_df = pd.read_csv("performance.csv")
+        new_performance_row = {
+            'Model Name': model_name,
+            'Accuracy Territory': accuracy_ter,
+            'Precision Territory': precision_ter,
+            'Recall Territory': recall_ter,
+            'Accuracy Sector': accuracy_sec,
+            'Precision Sector': precision_sec,
+            'Recall Sector': recall_sec
+        }
+
+        # Check if the model already exists in the dataframe
+        if model_name in performance_df['Model Name'].values:
+            performance_df.update(performance_df.loc[performance_df['Model Name'] == model_name, new_performance_row.keys()].assign(**new_performance_row))
+        else:
+            performance_df = performance_df.append(new_performance_row, ignore_index=True)
+
+        performance_df.to_csv("performance.csv", index=False)
+        st.write("Model performance updated in performance.csv.")
 
         st.write("Retraining and evaluation completed.")
 
